e6b7d76923d25291526253a78f30841595683d39
Subhajit Patra <1souvikpa@gmail.com>
2021-06-24 21:40:57 +0530

    built counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount]= useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick= {()=>{
+        setCount(count-1);
+      }} className="btn">
+        decrement
+      </button>
+      <div className="cnt">
+        {count}
+      </div>
+      <button onClick= {()=>{
+        setCount(count+1);
+      }} className="btn">
+        increment
+      </button>
     </div>
   );
 }
